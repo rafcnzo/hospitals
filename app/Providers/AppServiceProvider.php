ecda7bd5b3655eca7cc89fb9dbffea143bd9b0f7
rafcnzo <nanangdorris@gmail.com>
2025-12-13 18:57:10 +0700

    update providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Auth;
 
@@ -52,5 +53,9 @@ class AppServiceProvider extends ServiceProvider
             
             return false;
         });
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
